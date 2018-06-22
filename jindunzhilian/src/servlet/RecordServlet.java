8675f044cab730fdb11d6d8a1ce79b7bc6214eff
Ypf0428 <1041089840@qq.com>
2018-06-22 13:03:25 +0800

    第二次 账单查询 更新recordServlet

--- jindunzhilian/src/servlet/RecordServlet.java
+++ jindunzhilian/src/servlet/RecordServlet.java
@@ -6,34 +6,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.omg.PortableServer.REQUEST_PROCESSING_POLICY_ID;
 /**
- * Servlet implementation class RecordServlet
- */
+ * @author Y1041
+ * */
 public class RecordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecordServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		//按时间查询
+		String username = request.getParameter("username");
+		String selectWay = request.getParameter("selectway");
+		String plate = request.getParameter("plate");
+		String startTimeScope = request.getParameter("starttimescope");
+		String endTimeScope = request.getParameter("endtimescope");
+		String currentPage = request.getParameter("currentPage");
+		
+		
+		
 	}
 
 }
